3dce729fe7e0434e4d73813f0893d038dd917a0c
sala19a <pedroh.mix@gmail.com>
2022-05-16 20:56:57 -0300

    Atualizando status do pokemons

--- js/requestPokemon.js
+++ js/requestPokemon.js
@@ -1,4 +1,4 @@
-const idInicial = 1;
+const idInicial = 3;
 const idAnterior = idInicial - 1;
 const idPosterior = idInicial + 1;
 
@@ -7,6 +7,12 @@ this.imagemPokemonObject = document.getElementById("imagemPokemon")
 this.tipo1Pokemon = document.getElementById("tipo1Pokemon")
 this.tipo2Pokemon = document.getElementById("tipo2Pokemon")
 this.altura = document.getElementById("altura")
+this.hp = document.getElementById("hp")
+this.ataque = document.getElementById("ataque")
+this.defesa = document.getElementById("defesa")
+this.ataqueEspecial = document.getElementById("ataqueEspecial")
+this.defesaEspecial = document.getElementById("defesaEspecial")
+this.speed = document.getElementById("speed")
 
 findPokemonById(idInicial);
 
@@ -25,6 +31,15 @@ function findPokemonById(id) {
         tipo1Pokemon.textContent = response.types[0].type.name;
         tipo2Pokemon.textContent = response.types[1].type.name;
         altura.textContent = parseInt(response.height)/10;
+        hp.textContent = response.stats[0].base_stat;
+        ataque.textContent = response.stats[1].base_stat;
+        defesa.textContent = response.stats[2].base_stat;
+        ataqueEspecial.textContent = response.stats[3].base_stat;
+        defesaEspecial.textContent = response.stats[4].base_stat;
+        speed.textContent = response.stats[5].base_stat;
+
         console.log(response);
+
+
     });
 }
